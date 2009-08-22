b594c85aa24e3a8340f3a531f996d2d26ea6edd9
Philip (flip) Kromer <flip@infochimps.org>
2009-08-22 14:00:15 -0500

    Moving utils to bin dir

--- bin/test_run.rb
+++ bin/test_run.rb
@@ -4,17 +4,10 @@ require 'rubygems'
 require 'json'
 require 'edamame'
 
-# pq.hoard.each do |job|
-#   puts job.body
-# end
-
 broker = Edamame::Broker.new(
-  :store => { :type => 'TyrantStore',    :uri => ':11212'},
-  :queue => { :type => 'BeanstalkQueue', :uris => ['localhost:11210'] }
+  :queue => { :type => 'BeanstalkQueue', :uris => ['localhost:11210'] },
+  :store => { :type => 'TyrantStore',    :uri  =>           ':11212'  }
   )
-
-# broker.queue.load
-
 
 broker.work do |job|
   Monkeyshines.logger.info [job, job.scheduling, job.stats, job.obj].inspect
