43069581389473a8807a51d3a60b9b9933bcb315
Philip (flip) Kromer <flip@infochimps.org>
2009-08-22 23:24:48 -0500

    accordionize

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,3 +1,8 @@
+jQuery.fn.accordion = function(){
+  $(this).toggleClass("closed");
+  $(this).parent(".toggle").find('> :not(h2)').toggle();
+  console.log("clicked");
+}
 
 
 // ***************************************************************************
@@ -5,4 +10,7 @@
 //   Load all this stuff
 //
 $(document).ready(function(){
+  $(".toggle h2").click(function(){ $(this).accordion(); });
+  // $(".toggle h2").accordion();
+
 });
